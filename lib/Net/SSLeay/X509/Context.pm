0b67947a605854c2f23c416d55bd2ad7cdcfd189
Sam Vilain <sam.vilain@catalyst.net.nz>
2009-10-06 18:18:14 +1300

    Oh, go on, have POD then.
    Makes the pod coverage test quiet :)

--- lib/Net/SSLeay/X509/Context.pm
+++ lib/Net/SSLeay/X509/Context.pm
@@ -46,6 +46,49 @@ use Net::SSLeay::Functions 'x509_store_ctx';
 
 __END__
 
+=head1 NAME
+
+Net::SSLeay::X509::Context - wrapper for X509_STORE_CTX* pointers
+
+=head1 SYNOPSIS
+
+  # ... within callback installed by
+  #     Net::SSLeay::Context::set_verify ...
+  my $x509_ctx = Net::SSLeay::X509::Context->new(
+      x509_store_ctx => $x509_store_ctx,
+      );
+  my $cert = $x509_ctx->get_current_cert;
+
+=head1 DESCRIPTION
+
+This is a class which represents the X509_STORE_CTX* pointers; it is
+currently poorly understood, the best reference for understanding will
+be relevant functions within L<Net::SSLeay> (especially the main
+binding wrapper, F<SSLeay.xs>), and the OpenSSL source code.
+
+You should not need to use this class for regular use of the module;
+if you find a use for it, or would like to help complete or document
+it, please submit a patch or pull request.
+
+=head1 AUTHOR
+
+Sam Vilain, L<samv@cpan.org>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2009  NZ Registry Services
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the Artistic License 2.0 or later.  You should
+have received a copy of the Artistic License the file COPYING.txt.  If
+not, see <http://www.perlfoundation.org/artistic_license_2_0>
+
+=head1 SEE ALSO
+
+L<Net::SSLeay::OO>, L<Net::SSLeay::Context/set_verify>
+
+=cut
+
 # Local Variables:
 # mode:cperl
 # indent-tabs-mode: t
--- lib/Net/SSLeay/X509/Store.pm
+++ lib/Net/SSLeay/X509/Store.pm
@@ -27,6 +27,48 @@ use Net::SSLeay::Functions 'x509_store';
 
 1;
 
+__END__
+
+=head1 NAME
+
+Net::SSLeay::X509::Store - wrapper for X509_STORE* pointers
+
+=head1 SYNOPSIS
+
+ my $store = $ctx->get_cert_store;
+ $store->add_cert(...);
+ $store->set_purpose(...);
+
+=head1 DESCRIPTION
+
+This is a class which represents the X509_STORE* pointers; it is
+currently poorly understood, the best reference for understanding will
+be relevant functions within L<Net::SSLeay> (especially the main
+binding wrapper, F<SSLeay.xs>), and the OpenSSL source code.
+
+You should not need to use this class for regular use of the module;
+if you find a use for it, or would like to help complete or document
+it, please submit a patch or pull request.
+
+=head1 AUTHOR
+
+Sam Vilain, L<samv@cpan.org>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2009  NZ Registry Services
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the Artistic License 2.0 or later.  You should
+have received a copy of the Artistic License the file COPYING.txt.  If
+not, see <http://www.perlfoundation.org/artistic_license_2_0>
+
+=head1 SEE ALSO
+
+L<Net::SSLeay::OO>, L<Net::SSLeay::Context/get_cert_store>
+
+=cut
+
 # Local Variables:
 # mode:cperl
 # indent-tabs-mode: t
